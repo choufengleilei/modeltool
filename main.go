7f3a2fa26036b3bbc5c4a68d7bfc72415b75b5d7
choufengleilei <choufengleilei@126.com>
2020-12-24 11:19:04 +0800

    命令行执行

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"github.com/modeltool/generate"
+	"strings"
 )
 
 func main() {
-	//TODO 加入flag命令行执行
-
+	tables := flag.String("table", "", "mysql tableNames example: table_name1,table_name2")
+	flag.Parse()
+	var ts []string
+	if tables != nil {
+		ts = strings.Split(*tables, ",")
+	}
 	//generate.Generate() //生成所有表信息
-	generate.Generate(
-		"t_swap_order")
+	generate.Generate(ts...)
 }
